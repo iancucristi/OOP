7371993299ad3cf6d4dc69edfbb59a1ee03e3f6d
iancucristi <krystyke93@gmail.com>
2022-02-23 21:11:10 +0200

    no message

---
+++ src/ro/itschool/Cerc/Main.java
@@ -0,0 +1,10 @@
+package ro.itschool.Cerc;
+
+public class Main {
+    public static void main(String[] args) {
+        Cerc cerc =new Cerc();
+        cerc.setRaza(3);
+        double aria = cerc.getRaza();
+        System.out.println("aria cercului este " + aria);
+    }
+}
